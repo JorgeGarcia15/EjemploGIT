0619bf6c8685fe25ac51bd11183a5de4115a9848
JorgeGarcia15 <jordy615g@gmail.com>
2015-11-06 17:52:20 +0100

    Mi primera prueba
    JOEWPERF

---
+++ src/Ejemplo1.java
@@ -0,0 +1,17 @@
+
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+
+public class Ejemplo1 extends GraphicsProgram {
+
+	public void run(){
+		
+		GLabel etiqueta = new GLabel("Hola GIT");
+		
+		add(etiqueta, 120, 130);
+		
+		
+	}
+	
+	
+}
