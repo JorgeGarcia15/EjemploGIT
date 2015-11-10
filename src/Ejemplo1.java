db7601d31efc10a78900d33cea47e9ea469eb3b0
JorgeGarcia15 <jordy615g@gmail.com>
2015-11-10 17:47:14 +0100

    Segunda Prueba
    La piramide ya esta invertida

--- src/Ejemplo1.java
+++ src/Ejemplo1.java
@@ -1,17 +1,41 @@
 
 import acm.graphics.*;
-import acm.program.GraphicsProgram;
+import acm.program.*;
 
 public class Ejemplo1 extends GraphicsProgram {
 
+	private static final int ANCHO_LADRILLO = 30;
+	private static final int ALTO_LADRILLO = 12;
+	private static final int LADRILLOS_BASE = 14;
+
+	private static final int ALTO_PIRAMIDE = LADRILLOS_BASE * ALTO_LADRILLO;
+
+	int basePiramide = LADRILLOS_BASE * ANCHO_LADRILLO;
+	
+	
+	
 	public void run(){
-		
-		GLabel etiqueta = new GLabel("Hola GIT");
-		
-		add(etiqueta, 120, 130);
+
+		setSize(800, 800);
 		
 		
+		for(int j=0; j<LADRILLOS_BASE; j++){
+
+
+			for(int i=0; i<LADRILLOS_BASE-j; i++){
+				GRect ladrillo = new GRect(ANCHO_LADRILLO, ALTO_LADRILLO);
+
+				add (ladrillo,(getWidth()/2 - basePiramide/2 + ANCHO_LADRILLO*j/2 + ANCHO_LADRILLO * i), (getHeight() - ALTO_LADRILLO - ALTO_LADRILLO*j)-1);
+			}
+
+			pause (30);
+
+
+
+
+
+		}		
+
 	}
-	
-	
+
 }
